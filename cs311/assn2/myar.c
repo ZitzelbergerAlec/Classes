c2db390cff8a87f74e668798dc5426baf9b695e6
David Merrick <david@david-merrick.com>
2013-01-24 15:46:20 -0800

    Added skeleton functions for append and extract

--- cs311/assn2/myar.c
+++ cs311/assn2/myar.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int append(int numfiles){
+
+int append(char *filename){
+	printf("Appending: %s\n", filename);
 	return(0);
 }
 
 
+int extract(char *filename){
+	printf("Extracting from archive: %s\n", filename);
+	return(0);
+}
+
+
+
 int main(int argc, char* argv[]){
 	int c;
-	char *cvalue = NULL;
+	char *qvalue = NULL;
 	while((c = getopt(argc, argv, "q:x:tvd:Aw:")) != -1){
 		switch(c){
-			case 'q':
-				cvalue = optarg;
-				printf("cvalue = %s\n", cvalue);
+			case 'q': //quickly append named files to archive
+				append(optarg);
 				break;
-			case 'x':	
-				printf("You entered x!\n");
+			case 'x': //extract named files	
+				extract(optarg);
 				break;
-			case 't':	
+			case 't': //print a concise table of contents of the archive
 				printf("You entered t!\n");
 				break;
-			case 'v':	
+			case 'v': //print a verbose table of contents of the archive	
 				printf("You entered v!\n");
 				break;
-			case 'd':	
+			case 'd': //delete named files from archive
 				printf("You entered d!\n");
 				break;
-			case 'A':	
+			case 'A': //quickly append all ``regular'' files in the current directory (except the archive itself)	
 				printf("You entered A!\n");
 				break;
-			case 'w':	
+			case 'w': //Extra credit: for a given timeout, add all modified files to the archive (except the archive itself)	
 				printf("You entered w!\n");
 				break;
 			default:
