8a4b51e0b6ee23d9274006752fa04322c35776f6
David Merrick <david@david-merrick.com>
2013-01-24 16:53:37 -0800

    Swapped getopt for manually parsing argv to better meet specifications. Warning: code currently does not compile.

--- cs311/assn2/myar.c
+++ cs311/assn2/myar.c
@@ -3,49 +3,42 @@
 #include <unistd.h>
 
 
-int append(char *filename){
-	printf("Appending: %s\n", filename);
+int append(){
+	//printf("Appending: %s\n", filename);
+	printf("Appending\n");
 	return(0);
 }
 
 
-int extract(char *filename){
-	printf("Extracting from archive: %s\n", filename);
+int extract(){
+	//printf("Extracting from archive: %s\n", filename);
+	printf("Extracting!\n");
 	return(0);
 }
 
 
 
 int main(int argc, char* argv[]){
-	int c;
+	char *c;
 	char *qvalue = NULL;
-	while((c = getopt(argc, argv, "q:x:tvd:Aw:")) != -1){
-		switch(c){
-			case 'q': //quickly append named files to archive
-				append(optarg);
-				break;
-			case 'x': //extract named files	
-				extract(optarg);
-				break;
-			case 't': //print a concise table of contents of the archive
-				printf("You entered t!\n");
-				break;
-			case 'v': //print a verbose table of contents of the archive	
-				printf("You entered v!\n");
-				break;
-			case 'd': //delete named files from archive
-				printf("You entered d!\n");
-				break;
-			case 'A': //quickly append all ``regular'' files in the current directory (except the archive itself)	
-				printf("You entered A!\n");
-				break;
-			case 'w': //Extra credit: for a given timeout, add all modified files to the archive (except the archive itself)	
-				printf("You entered w!\n");
-				break;
-			default:
-				printf("You didn't enter anything!\n");
-				break;
-		}
-	}
+	c = argv[1];
+	switch(c){
+		case "-q": //quickly append named files to archive
+			append();
+		case "-x": //extract named files	
+			extract();
+		case "-t": //print a concise table of contents of the archive
+			printf("You entered t!\n");
+		case "-v": //print a verbose table of contents of the archive	
+			printf("You entered v!\n");
+		case "-d": //delete named files from archive
+			printf("You entered d!\n");
+		case "-A": //quickly append all ``regular'' files in the current directory (except the archive itself)	
+			printf("You entered A!\n");
+		case "-w": //Extra credit: for a given timeout, add all modified files to the archive (except the archive itself)	
+			printf("You entered w!\n");
+		default:
+			printf("You didn't enter anything!\n");
+	};
 	return(0);
 }
