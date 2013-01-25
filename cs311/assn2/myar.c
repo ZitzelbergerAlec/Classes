fdac49bd41ac51350c5c1ade923eaeac10211f21
David Merrick <david@david-merrick.com>
2013-01-25 09:37:45 -0800

    Rewrote switch-case to a bunch of if statements, completed skeleton code for extract/help/append functions.

--- cs311/assn2/myar.c
+++ cs311/assn2/myar.c
@@ -10,35 +10,42 @@ int append(){
 }
 
 
-int extract(){
+int extract(char *argv[]){
 	//printf("Extracting from archive: %s\n", filename);
 	printf("Extracting!\n");
+	printf("argv1: %s\n", argv[1]);
+	printf("Archive file to use is %s\n", argv[2]);
 	return(0);
 }
 
+void help(){ //prints usage of program
+	printf("Usage: myar -key archive filename ...\n");
+}
 
 
 int main(int argc, char* argv[]){
+	//Order of arguments:
+	//myar key afile name ...
+	// where key is "-q" etc
 	char *c;
-	char *qvalue = NULL;
-	c = argv[1];
-	switch(c){
-		case "-q": //quickly append named files to archive
-			append();
-		case "-x": //extract named files	
-			extract();
-		case "-t": //print a concise table of contents of the archive
-			printf("You entered t!\n");
-		case "-v": //print a verbose table of contents of the archive	
-			printf("You entered v!\n");
-		case "-d": //delete named files from archive
-			printf("You entered d!\n");
-		case "-A": //quickly append all ``regular'' files in the current directory (except the archive itself)	
-			printf("You entered A!\n");
-		case "-w": //Extra credit: for a given timeout, add all modified files to the archive (except the archive itself)	
-			printf("You entered w!\n");
-		default:
-			printf("You didn't enter anything!\n");
-	};
+	c = argv[2]; // Set to 2 for debugging purposes. Set this back to 1 later!!;
+	if(!strcmp(c,"-q")){ //quickly append named files to archive
+		append();
+	} else if(!strcmp(c,"-x")){
+		extract(argv);	
+	} else if(!strcmp(c,"-v")){
+                printf("-v\n");
+        } else if(!strcmp(c,"-d")){
+                printf("-d\n");
+        } else if(!strcmp(c,"-A")){
+                printf("-A\n");
+        } else if(!strcmp(c,"-w")){
+                printf("-w\n");
+        } else if(!strcmp(c,"-h")){
+                help();
+        }  else {
+		help();
+	}
+	
 	return(0);
 }
