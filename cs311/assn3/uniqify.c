2e91ac58664e0eedc412b0bf269de4057f4439f2
David Merrick <david@david-merrick.com>
2013-02-06 19:56:51 -0800

    Added help function. parsing input isn't working but am gonna skip that for now and assume input is in csv format

--- cs311/assn3/uniqify.c
+++ cs311/assn3/uniqify.c
@@ -1,23 +1,36 @@
+/*
+ * Notes:
+ * cast everything to lowercase
+ * have command line argument for number of processes
+ * 
+ *
+ */ 
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-char *scanword(char *filename);
-
+void help();
 
 int main(int argc, char **argv){	
-	char *scanned;
-	scanned = scanword(argv[1]);
-	printf("Scanned = %s\n", scanned);
-	return(0);
-}
-
-char *scanword(char *filename){
 	FILE *fp;
-	if((fp=fopen(filename, "r")) == NULL) {
+	if((fp=fopen(argv[1], "r")) == NULL) {
     	printf("Cannot open file.\n");
     	exit(1);
   	}
-	char *word = (char *) malloc(sizeof(char) * 100);
-	fscanf(fp, "%[^0-9^ ]", word); //segfaults here
-	return word;
+	char word[100];
+	int output;
+	output = 1;
+	while(fscanf(fp, "%[^0-9,\t\n\v\f]", word) > 0){
+		printf("%s\n", word);
+	}	
+	return(0);
+}
+
+void help(){
+	printf("Usage:\n");
+	printf("uniqify [filename] [number of sorting processes]\n");
+	exit(0);
 }
