75a37cffef03412dea62eab7e423a8b27cc7cbcb
David Merrick <david@david-merrick.com>
2013-02-08 15:44:34 -0800

    Added code for suppressor pipes. But not working. So gonna revoke. But wanted to save on here.

--- cs311/assn3/uniqify.c
+++ cs311/assn3/uniqify.c
@@ -18,6 +18,7 @@
 //Function prototypes
 void 	closePipe(int pfd);
 void 	createPipe(int *fds);
+int 	*generatePipes(int numpipes);
 void 	help();
 void 	PukeAndExit(char *errormessage);
 
@@ -40,14 +41,23 @@ int main(int argc, char **argv){
              	break;
     }
 
-    int numpipes = numsorts * 2;
+    int num_sortpipes = numsorts * 2; //number of pipes for sort
+    int num_suppipes = numsorts * 2; //number of pipes for suppressor
 	int processarray[numsorts]; //Array of processes
 	
-	//Generate all necessary pipes
+	//Generate all necessary pipes for sort
 	//So all read ends will be even
-	int pipefds[2 * numpipes];
-	for(i = 0; i < numpipes; i++){
-    	if(pipe(pipefds + i*2) < 0 ){
+	int sortpipefds[2 * num_sortpipes];
+	for(i = 0; i < num_sortpipes; i++){
+    	if(pipe(sortpipefds + i*2) < 0){
+        	PukeAndExit("Error creating pipes\n");
+    	}
+	}
+
+	//Generate all necessary pipes for suppressor
+	int suppipefds[2 * num_suppipes];
+	for(i = 0; i < num_suppipes; i++){
+    	if(pipe(suppipefds + i*2) < 0){
         	PukeAndExit("Error creating pipes\n");
     	}
 	}
@@ -59,13 +69,20 @@ int main(int argc, char **argv){
 		pid = fork();
 		processarray[i] = pid;
 		if(pid == 0){ //child case
-			printf("Spawning sort process \n");
+			printf("Sort process spawned\n");
 			//Bind stdin to sort process
-			closePipe(pipefds[j+1]); //close write end
-			if (pipefds[j] != STDIN_FILENO){ //Defensive check
-				if (dup2(pipefds[j], STDIN_FILENO) == -1)
+			closePipe(sortpipefds[j+1]); //close write end
+			if(sortpipefds[j] != STDIN_FILENO){ //Defensive check
+				if(dup2(sortpipefds[j], STDIN_FILENO) == -1)
 					PukeAndExit("dup2 0");
-				closePipe(pipefds[j]);
+				closePipe(sortpipefds[j]);
+			}
+			closePipe(suppipefds[j]);
+			//Bind stdout to suppressor pipe
+			if(suppipefds[j+1] != STDOUT_FILENO){ //Defensive check
+				if(dup2(suppipefds[j+1], STDOUT_FILENO) == -1)
+					PukeAndExit("dup2 1");
+				closePipe(suppipefds[j+1]);
 			}
             execlp("sort", "sort", (char *)NULL);
 		}
@@ -73,43 +90,69 @@ int main(int argc, char **argv){
 	}
 
 	//Close read end of pipes in parent
-	for(i = 0; i < numpipes; i+=2){
-    	closePipe(pipefds[i]);
+	for(i = 0; i < num_sortpipes; i+=2){
+    	closePipe(sortpipefds[i]);
 	}
 
 	//Fopen all output pipes
-	j = 0; //counter for outputFiles array
-	FILE *outputFiles[numpipes/2];
-	for(i=1; i < numpipes; i+= 2){
-		outputFiles[j] = fdopen(pipefds[i], "w");
+	j = 0; //counter for outputs array
+	FILE *outputs[num_sortpipes/2];
+	for(i=1; i < num_sortpipes; i+= 2){
+		outputs[j] = fdopen(sortpipefds[i], "w");
 		j++;
 
+	}
+
+	//Fopen all input pipes to suppressor
+	j = 0; //counter for inputs array
+	FILE *inputs[num_suppipes/2];
+	for(i=0; i < num_suppipes; i+= 2){
+		inputs[j] = fdopen(suppipefds[i], "r");
+		j++;
 	}
 
 	//Distribute words to them
 	i = 0;
 	while(scanf("%[^,]%*c,", word) != EOF){
 		i++;
-		fputs(word, outputFiles[i % numsorts]); //round robin
-		fputs("\n", outputFiles[i % numsorts]); //sort needs newline
+		fputs(word, outputs[i % numsorts]); //round robin
+		fputs("\n", outputs[i % numsorts]); //sort needs newline
+	}
+
+	//Read stuff to test if sort processes are outputting to correct pipes
+	//Close write end of suppressor pipes in parent
+	//Close read end of pipes in parent
+	for(i = 1; i < num_suppipes; i+=2){
+    	closePipe(suppipefds[i]);
+	}
+
+	char buf2[10];
+	for(i=0; i<num_suppipes;i++){
+		fgets(buf2, 10, inputs[i]); //hangs here
+		printf("buf = %s\n", buf2);
 	}
 
 	//Flush the pipes:
-	//Close output files
-	for(i=1; i < numpipes/2; i++){
-		fclose(outputFiles[i]);
+	//Close output pipes and input pipes
+	for(i=1; i < num_sortpipes/2; i++){
+		fclose(outputs[i]);
+		fclose(inputs[i]);
 	}
 
 	//Close pipes
+	/*
 	for(i=1; i<numpipes; i+=2){
 		closePipe(pipefds[i]);
 	}
+	*/
 
 	//Reap all zombies
 	int status;
+	/*
 	for(i=0; i < numsorts; i++){
 		waitpid(processarray[i], &status, 0);
 	}
+	*/
 
 	return(0);
 }
