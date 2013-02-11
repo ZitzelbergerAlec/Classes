8260cdddeeafe75da588e59cccf2c0c1d44f7682
David Merrick <david@david-merrick.com>
2013-02-11 10:43:37 -0800

    It's mostly working! There's just a slight bug in the printWords function where it doesn't pick up on duplicate words or count them sometimes.

--- cs311/assn3/uniqify.c
+++ cs311/assn3/uniqify.c
@@ -20,25 +20,27 @@
 
 #define MAX_WORD_LEN 100 //maximum word length
 
-typedef struct wordCounter{
+struct wordCounter{
 	char word[MAX_WORD_LEN];
 	int count;
-} curWord;
+};
 
 //debug
 void DebugPrintWords(int numWords, char **words);
 
 //Function prototypes
+void  	alphWords(int numWords, char **words);
 void 	clearWords(int numWords, char **words);
 void 	closePipe(int pfd);
 void 	createPipe(int *fds);
 int 	**generatePipesArray(int numpipes);
 void 	help();
-void  	alphWords(int numWords, char **words);
-void 	printWords(int numWords, char **words, struct wordCounter curWord);
+int 	isEmpty(char *str);
+void 	printWords(int numWords, char **words, struct wordCounter *curWord);
 void 	PukeAndExit(char *errormessage);
 void 	RRParser(int numsorts, int **outPipe);
 void  	spawnSorts(int numsorts, int **inPipe, int **outPipe);
+void 	stripNewline(char *word);
 void 	strtoupper(char *str);
 void 	spawnSuppressor(int numsorts, int **inPipe);
 void 	suppressorProcess(int numsorts, int **inPipe);
@@ -199,63 +201,53 @@ void spawnSuppressor(int numsorts, int **inPipe){
 void suppressorProcess(int numsorts, int **inPipe){
 	int i;
 	char buf[MAX_WORD_LEN]; 
-	char *words[MAX_WORD_LEN];
-
-	//Fopen all input pipes
+	char **words;
 	FILE *inputs[numsorts];
-	for(i=0; i < numsorts; i++){
-		inputs[i] = fdopen(inPipe[i][0], "r");
-		if(inputs[i] == NULL)
-			printf("Error: could not create input stream.\n");
-	}
+	struct wordCounter *curWord = (struct wordCounter *) malloc(sizeof(struct wordCounter));
+	curWord->count = 0;
+
+        //initialize word array
+        words = (char**)malloc(numsorts * sizeof(char*));
+        for (i = 0; i < numsorts; i++) {
+                words[i] = (char*)malloc(MAX_WORD_LEN * sizeof(char));
+        }
+
+        //fdopen inPipes
+        for(i = 0; i < numsorts; i++) {
+                inputs[i] = fdopen(inPipe[i][0], "r");
+        }
+
+        i = 0;
+        int j = 0;
+        int nullCount = 0;
+        while(1){
+        	if(fgets(words[j], MAX_WORD_LEN, inputs[i % numsorts]) == NULL){
+                        strncpy(words[j], "0", MAX_WORD_LEN); //mark words
+                        nullCount++;
+                        if(nullCount == numsorts)
+                        	break;
+                } else {
+                	stripNewline(words[j]);
+                	if(isEmpty(words[j]))
+                		continue;
+                }
+                j++;
+                if(!(j < numsorts)){
+                	alphWords(j, words);
+                	printWords(j, words, curWord);
+                	clearWords(j, words);
+                	j = 0;
+                }
+        	i++;
+        }
 
 
-	int j; //index for words and inner for loop, respectively
-	i=j=0; //i is the index for pipes
-	
-	//To do: make this struct
-	//structure for counting words
-	struct wordCounter curWord;
-	
-	//To do: This loop should have a NULL counter instead that counts the number of NULLS at every go around
-	//That way it won't break out too early if one of the pipes is empty but others still have data.
-	//If the number of NULLS is equal to numsorts, break.
-
-	int nullCount = 0; //counts number of nulls from fgets
-	clearWords(numsorts, words); //initialize word array
-	
-
-	if(fgets(words[0], MAX_WORD_LEN, inputs[0]) != NULL)
-		printf("words[0] = %s\n", words[0]);
-
-	/*
-	while(1){
-		if(fgets(words[j], MAX_WORD_LEN, inputs[i % numsorts]) == NULL){
- 			nullCount++;
-			if(nullCount == numsorts){
-				//alphWords(j+1, words);
-				//DebugPrintWords(j+1, words);
-				//printWords(j+1, words, curWord); //print leftovers
-				break;
-			} else {
-				i++;
-				continue;
-			}
-		}
-		if(!(j < numsorts)){
-			//DebugPrintWords(j, words);
-			//alphWords(j+1, words); //this causes a segfault
-			//DebugPrintWords(j+1, words);
-			//printWords(j, words, curWord);
-			//clearWords(j, words);
-			j = 0;
-		}
-		//printf("words[0] = %s\n", words[0]);
-		//printf("words[1] = %s\n", words[1]);
-		j++;
-		i++;
-	}
-	*/
+        //Free words array
+        for (i = 0; i < numsorts; i++) {
+                free(words[i]);
+        }
+        free(words);
+        free(curWord);
 
 	//Close inputs
 	for(i=0; i < numsorts; i++){
@@ -263,38 +255,53 @@ void suppressorProcess(int numsorts, int **inPipe){
 	}
 }
 
+int isEmpty(char *str){ //returns 1 if a string is empty
+	if(str[0] == '\0')
+		return 1;
+	return 0;
+}
+
 void DebugPrintWords(int numWords, char **words){
 	//debugs words array by printing them out
 	int i;
 	for(i=0; i<numWords; i++){
+		if(!strcmp(words[i], "0"))
+			continue;
 		printf("words[%d] = %s", i, words[i]);
 	}
 }
 
-void printWords(int numWords, char **words, struct wordCounter curWord){
+void printWords(int numWords, char **words, struct wordCounter *curWord){
 	//Print the unique words and their counts
 	int i = 0;
-	if(curWord.count == 0){ //curWord is uninitializied; this is first time through loop
-		//curWord.word = words[0];
-		curWord.count = 1;
+	if(curWord->count == 0){ //curWord is uninitializied; this is first time through loop
+		strncpy(curWord->word, words[0], MAX_WORD_LEN);
+		curWord->count = 1;
 		i = 1;
 	}
 	for(i;i<numWords;i++){
-		if(!strcmp(words[i],curWord.word)){
-			curWord.count++;
+		if(!strcmp(words[i], "0"))
+			continue;
+		if(!strcmp(words[i],curWord->word)){
+			curWord->count++;
 		} else {
-			printf("%s %d\n", curWord.word, curWord.count);
-			//curWord.word = words[i];
-			curWord.count = 1;
+			printf("%s %d\n", curWord->word, curWord->count);
+			strncpy(curWord->word, words[i], MAX_WORD_LEN);
+			curWord->count = 1;
 		}
 	}
+}
+
+void stripNewline(char *word){ //removes trailing newline characters from strings
+	if (word[strlen(word) - 1] == '\n')
+  		word[strlen(word) - 1] = '\0';
 }
 
 void clearWords(int numWords, char **words){
 	//Clears the array of words
 	int i; 
 	for(i = 0; i < numWords; i++){
-		words[i] = "0";
+		strncpy(words[i], "0", MAX_WORD_LEN);
 	}
 }
 
@@ -305,12 +312,10 @@ void alphWords(int numWords, char **words){ //Alphabetizes words
 	int alpha; //index of next lowest alphabetical word
 	//Perform an insertion sort
 	for(j=0; j < numWords; j++){
-		if(!strcmp(words[j], "0"))
-				break;
 		alpha = j;
 		for(i = j; i < numWords; i++){
 			if(!strcmp(words[i], "0"))
-				break;
+				continue;
 			if(strcmp(words[i],words[alpha]) < 0)
 				alpha = i; //found a new alpha word
 		}
