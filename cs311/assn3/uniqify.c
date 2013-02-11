232442b48407011c61e0c430eb2c581f5a15e3c0
David Merrick <david@david-merrick.com>
2013-02-11 09:43:44 -0800

    Suppressor is currently broken. About to do some major work to fix but am committing what I have now.

--- cs311/assn3/uniqify.c
+++ cs311/assn3/uniqify.c
@@ -1,8 +1,8 @@
 /*
  * Notes:
- * cast everything to lowercase
+ * cast everything to uppercase
  * have command line argument for number of processes
- * 
+ * To do: signal handling
  *
  */ 
 
@@ -20,12 +20,22 @@
 
 #define MAX_WORD_LEN 100 //maximum word length
 
+typedef struct wordCounter{
+	char word[MAX_WORD_LEN];
+	int count;
+} curWord;
+
+//debug
+void DebugPrintWords(int numWords, char **words);
+
 //Function prototypes
+void 	clearWords(int numWords, char **words);
 void 	closePipe(int pfd);
 void 	createPipe(int *fds);
 int 	**generatePipesArray(int numpipes);
 void 	help();
-void  	mergeWords(int numwords, char **words);
+void  	alphWords(int numWords, char **words);
+void 	printWords(int numWords, char **words, struct wordCounter curWord);
 void 	PukeAndExit(char *errormessage);
 void 	RRParser(int numsorts, int **outPipe);
 void  	spawnSorts(int numsorts, int **inPipe, int **outPipe);
@@ -142,11 +152,10 @@ void spawnSorts(int numsorts, int **inPipe, int **outPipe){
 void RRParser(int numsorts, int **outPipe){ //Round Robin parser
 	int i;
 	char word[MAX_WORD_LEN];
-	int readnum;
     	char buf[1];
     	i = 0;    
-    	while ((readnum = read(STDIN_FILENO, buf, 1)) != 0) {
-	        if (isalpha(buf[0])) {
+    	while(read(STDIN_FILENO, buf, 1) != 0) {
+	        if(isalpha(buf[0])){
 	            	buf[0] = toupper(buf[0]);
 	        } else {
 	                buf[0] = '\n';
@@ -190,7 +199,7 @@ void spawnSuppressor(int numsorts, int **inPipe){
 void suppressorProcess(int numsorts, int **inPipe){
 	int i;
 	char buf[MAX_WORD_LEN]; 
-	char *words[numsorts];
+	char *words[MAX_WORD_LEN];
 
 	//Fopen all input pipes
 	FILE *inputs[numsorts];
@@ -201,64 +210,52 @@ void suppressorProcess(int numsorts, int **inPipe){
 	}
 
 
-	int j,k; //index for words and inner for loop, respectively
-	i=j=k=0; //i is the index for pipes
+	int j; //index for words and inner for loop, respectively
+	i=j=0; //i is the index for pipes
 	
 	//To do: make this struct
 	//structure for counting words
-	struct wordCounter{
-		char word[MAX_WORD_LEN];
-		int count;
-	} curWord;
+	struct wordCounter curWord;
 	
 	//To do: This loop should have a NULL counter instead that counts the number of NULLS at every go around
 	//That way it won't break out too early if one of the pipes is empty but others still have data.
 	//If the number of NULLS is equal to numsorts, break.
 
 	int nullCount = 0; //counts number of nulls from fgets
-	while(1){		
-		if(fgets(buf, MAX_WORD_LEN, inputs[i % numsorts]) == NULL){
-			nullCount++;
+	clearWords(numsorts, words); //initialize word array
+	
+
+	if(fgets(words[0], MAX_WORD_LEN, inputs[0]) != NULL)
+		printf("words[0] = %s\n", words[0]);
+
+	/*
+	while(1){
+		if(fgets(words[j], MAX_WORD_LEN, inputs[i % numsorts]) == NULL){
+ 			nullCount++;
 			if(nullCount == numsorts){
+				//alphWords(j+1, words);
+				//DebugPrintWords(j+1, words);
+				//printWords(j+1, words, curWord); //print leftovers
 				break;
+			} else {
+				i++;
+				continue;
 			}
 		}
-		i++;
-		if(buf == NULL){ //Don't allow NULL words to be passed into mergeWords
-			continue;
-		}
-		printf("j = %d\n", j);
-		j++;
-		if(j < numsorts){
-			words[j] = buf;
-			printf("words[%d] = %s\n", j, words[j]);
-		} else {
-			k=0;
-			//mergeWords(j, words); //this causes a segfault
-			printf("words[%d] = %s\n", j, words[j]);
-			if(i == numsorts){ //set initial curWord
-				//printf("words[0] = %s\n", words[0]);
-				//strncpy(curWord.word, words[0], MAX_WORD_LEN);
-				curWord.count = 1;
-				k=1; //Start at index 1
-				//printf("curWord.word = %s\n", curWord.word);
-			}
-			/*
-			for(k;k<j;k++){
-				if(!strcmp(words[k],curWord.word)){
-					curWord.count++;
-				} else {
-					printf("%s %d\n", curWord.word, curWord.count);
-					curWord.word = words[k];
-					curWord.count = 1;
-				}
-			}
-			*/
+		if(!(j < numsorts)){
+			//DebugPrintWords(j, words);
+			//alphWords(j+1, words); //this causes a segfault
+			//DebugPrintWords(j+1, words);
+			//printWords(j, words, curWord);
+			//clearWords(j, words);
 			j = 0;
-			words[j] = buf;
 		}
+		//printf("words[0] = %s\n", words[0]);
+		//printf("words[1] = %s\n", words[1]);
+		j++;
+		i++;
 	}
-	//To do: merge leftovers
+	*/
 
 	//Close inputs
 	for(i=0; i < numsorts; i++){
@@ -266,15 +263,54 @@ void suppressorProcess(int numsorts, int **inPipe){
 	}
 }
 
-void mergeWords(int numwords, char **words){
-	int i, j;
+void DebugPrintWords(int numWords, char **words){
+	//debugs words array by printing them out
+	int i;
+	for(i=0; i<numWords; i++){
+		printf("words[%d] = %s", i, words[i]);
+	}
+}
+
+void printWords(int numWords, char **words, struct wordCounter curWord){
+	//Print the unique words and their counts
+	int i = 0;
+	if(curWord.count == 0){ //curWord is uninitializied; this is first time through loop
+		//curWord.word = words[0];
+		curWord.count = 1;
+		i = 1;
+	}
+	for(i;i<numWords;i++){
+		if(!strcmp(words[i],curWord.word)){
+			curWord.count++;
+		} else {
+			printf("%s %d\n", curWord.word, curWord.count);
+			//curWord.word = words[i];
+			curWord.count = 1;
+		}
+	}
+}
+
+void clearWords(int numWords, char **words){
+	//Clears the array of words
+	int i; 
+	for(i = 0; i < numWords; i++){
+		words[i] = "0";
+	}
+}
+
+void alphWords(int numWords, char **words){ //Alphabetizes words
 	//Takes an unorganized array of words
 	//Returns one organized in alphabetical order
+	int i, j;
 	int alpha; //index of next lowest alphabetical word
 	//Perform an insertion sort
-	for(j=0; j < numwords; j++){
+	for(j=0; j < numWords; j++){
+		if(!strcmp(words[j], "0"))
+				break;
 		alpha = j;
-		for(i = j; i < numwords; i++){
+		for(i = j; i < numWords; i++){
+			if(!strcmp(words[i], "0"))
+				break;
 			if(strcmp(words[i],words[alpha]) < 0)
 				alpha = i; //found a new alpha word
 		}
