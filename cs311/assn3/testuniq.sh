500c148a8a2e3b1fb11860f6cf403cf01fad5fed
David Merrick <david@david-merrick.com>
2013-02-15 11:04:49 -0800

    fixed output bug

--- cs311/assn3/testuniq.sh
+++ cs311/assn3/testuniq.sh
@@ -5,15 +5,14 @@
 # if no subfolder, just use "."
 
 function p {
-	local STR="$1"
-	STR+=", "
-	STR+=$(stat $1 --printf=%s)
-	STR+=", "
-	STR+=$(/usr/bin/time -f "%S" uniqify 10 < $1 > /dev/null)
+	local STR=""
+	STR+=$(du -sk $1 | cut -f 1) #get filesize in kb
+	STR+=", "	
 	echo $STR
+	/usr/bin/time -f "%S" uniqify 10 < $1 > /dev/null
 }
 
-echo "Filename, size (bytes), system time"
+echo "Fileize (kilobytes), system time"
 for f in $1/*txt
 	do 
 		p $f
