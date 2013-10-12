248fdbdb76474c22bd6766b8871a9fdce69e49ed
David Merrick <david@david-merrick.com>
2013-10-11 17:46:56 -0700

    updates to fix bug when first number is negative

--- CS 325/Assn 1/assignment1b.c
+++ CS 325/Assn 1/assignment1b.c
@@ -19,10 +19,11 @@ struct sum_struct{
 
 int main()
 {
-	int myarr[] = {5, -7, 7, -2};
-	close_to_zero(myarr, 4);
-	printf("Minimum sum is %d\n", min_sum.sum);
+    int myarr[] = {-360,948,-29,-447,515,-53,-491,-761,-719,-854,-77,-677,473,262,-95,844,-84,-875,241,-320, 143,888,-524,-947,-652,260,503,882,-692,223,745,-245,37,-387,602,862,125,653,938,635,-960,-375,555,-730,-198,-691,-74,336,112,62,-319,-877,-416,-646,-207,616,608,737,-143,-747,-387,788,105,258,408,517,451,-718,358,118,-738,72,-457,-551,-726,580,-297,-662,-135,220,-267,773,117,669,905,152,-789,-243,-901,-241,-223,380,668,-514,-916,723,957,-222,605,-371};
+	close_to_zero(myarr, 100);
+    printf("Minimum sum is %d\n", min_sum.sum);
 	printf("Found between indices %d and %d\n", min_sum.min, min_sum.max);
+	printf("Corresponding to %d and %d\n", myarr[min_sum.min], myarr[min_sum.max]);
 	return 0;
 }
 
@@ -31,13 +32,14 @@ void close_to_zero(int *arr, int size)
 {
     int temp_sum;
     int compare_sum; //variable to hold absolute value
-    min_sum.sum = arr[0]; //set initial value
+    min_sum.sum = abs(arr[0]); //set initial value
     min_sum.min = 0;
     min_sum.max = 0;
 	for(int i = 0; i < size; i++){
     	temp_sum = 0;
-    	if(abs(arr[i]) < min_sum.sum){
-      		min_sum.sum = arr[i];
+    	compare_sum = abs(arr[i]);
+        if(compare_sum < min_sum.sum){
+      		min_sum.sum = compare_sum;
       		min_sum.min = i;
       		min_sum.max = i;
     	}
