8ab7170c8a82c427150d7cff45d044d676341d28
David Merrick <david@david-merrick.com>
2013-10-11 17:13:40 -0700

    Implemented first algorithm

--- CS 325/Assn 1/assignment1a.c
+++ CS 325/Assn 1/assignment1a.c
@@ -19,8 +19,8 @@ struct sum_struct{
 
 int main()
 {
-	int myarr[] = {5, -7, 7, -2};
-	close_to_zero(myarr, 4);
+	int myarr[] = {5, -7, 2};
+	close_to_zero(myarr, 3);
 	printf("Minimum sum is %d\n", min_sum.sum);
 	printf("Found between indices %d and %d\n", min_sum.min, min_sum.max);
 	return 0;
@@ -41,15 +41,17 @@ void close_to_zero(int *arr, int size)
       		min_sum.min = i;
       		min_sum.max = i;
     	}
-    	temp_sum = arr[i];
     	for(int j = i+1; j < size; j++){
-      		temp_sum += arr[j];
+	    	temp_sum = arr[i];
+            for(int k = i+1; k <= j; k++){
+                temp_sum += arr[k];
+            }
             compare_sum = abs(temp_sum);
-      		if(compare_sum < min_sum.sum){
-				min_sum.sum = compare_sum;
-				min_sum.min = i;
-				min_sum.max = j;
-      		}
-    	}
+            if(compare_sum < min_sum.sum){
+                min_sum.sum = compare_sum;
+                min_sum.min = i;
+                min_sum.max = j;
+            }
+        }
     }
 }
